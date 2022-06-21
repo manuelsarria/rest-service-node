18121574b9d16972be9e6ea8d19cbc7b10266ec8
Manuel Sarrria <manuelsarria@Manuels-MacBook-Pro.local>
2022-06-21 08:26:40 -0500

    CRUD categorias

--- models/server.js
+++ models/server.js
@@ -8,8 +8,12 @@ class Server{
     constructor() {
         this.app = express();
         this.port = process.env.PORT;
-        this.usersPath = '/api/users';
-        this.authPath = '/api/auth';
+
+        this.paths = {
+            auth:       '/api/auth',
+            categorias: '/api/categorias',
+            usuarios:   '/api/usuarios'
+        }
 
         //conectar db
         this.conectarDB();
@@ -37,8 +41,9 @@ class Server{
     }
     
     routes() {
-        this.app.use(this.authPath, require('../routes/auth'));
-        this.app.use(this.usersPath, require('../routes/user'));
+        this.app.use(this.paths.auth, require('../routes/auth'));
+        this.app.use(this.paths.categorias, require('../routes/categorias'));
+        this.app.use(this.paths.usuarios, require('../routes/user'));
 
     }
 
